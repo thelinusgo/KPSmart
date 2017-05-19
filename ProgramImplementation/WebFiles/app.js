df5f512ff4567f9e2f1ce09f13006840952de498
Linus Go <Linus Go>
2017-05-19 22:38:56 +1200

    done basic HTML for login page and basic angular callbacks

--- ProgramImplementation/WebFiles/app.js
+++ ProgramImplementation/WebFiles/app.js
@@ -5,6 +5,29 @@ var app = angular.module('plunker', []);
 //list of users that can log in to KPSmart
 
 app.controller('MainCtrl', function($scope) {
-    var userTypes = ['CEO', 'Manager' ,'Regular User'];
+    $scope.userTypes = ['CEO', 'Manager' ,'Regular User'];
     $scope.name = 'World';
+
+    $scope.testUser = "userman";
+    $scope.testPwd = "password1";
+
+
+    $scope.checkLogin = function(){
+        if($scope.username == "" && $scope.password == ""){
+            $scope.print="Please input a valid username and password";
+        }else if($scope.username == "" && $scope.password != ""){
+            $scope.print="Please input a valid username";
+        }else if($scope.password == ""){
+            $scope.print="Please input a valid password";
+        }else if($scope.username == $scope.testUser && $scope.password == $scope.testPwd){
+            alert("Logged in successfully as " + testUser);
+        }
+    };
+
+    $scope.cancelLogin = function(){
+        $scope.username = "";
+        $scope.password = "";
+    }
+
+
 });
