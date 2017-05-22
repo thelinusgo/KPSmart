fae9dcf99233f2f9ef9e0d187f2d3d4eb10d01df
Linus Go <golinu@ecs.vuw.ac.nz>
2017-05-23 11:30:32 +1200

    added most of the HTML files for the various views

--- ProgramImplementation/WebFiles/app.js
+++ ProgramImplementation/WebFiles/app.js
@@ -4,6 +4,19 @@
 var app = angular.module('plunker', []);
 //list of users that can log in to KPSmart
 
+
+/*Code for the sidebar opening-closing logic */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+
 app.controller('MainCtrl', function($scope) {
     $scope.userTypes = ['CEO', 'Manager' ,'Regular User'];
     $scope.name = 'World';
