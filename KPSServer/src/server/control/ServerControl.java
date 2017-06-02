ffb9e69b7c8c25ce1ca826f6bfac7622de0b2edb
Christiandel Rabe <chris.rabe14@gmail.com>
2017-06-02 12:47:33 +1200

    created a database initialiser

--- KPSServer/src/server/control/ServerControl.java
+++ KPSServer/src/server/control/ServerControl.java
@@ -6,6 +6,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
+import server.handlers.initialisers.DatabaseInitialiser;
 import server.handlers.operations.Operations;
 import server.handlers.websocket.KPSWebSocketHandler;
 
@@ -15,12 +16,15 @@ public class ServerControl {
 	private Server server;
 
 	private JTextArea logPanel = null;
+	private DatabaseInitialiser dbInitialiser = new DatabaseInitialiser();
 
 	public ServerControl(JTextArea logPanel) {
 		this.logPanel = logPanel;
 	}
 
 	public void start(String port) throws Exception {
+		dbInitialiser.initialiseDatabase();
+		
 		if (port == null || !port.matches("[-+]?\\d*\\.?\\d+")) {
 			return;
 		}
