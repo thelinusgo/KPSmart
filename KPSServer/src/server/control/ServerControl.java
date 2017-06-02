afc188e00de2707fc315593a1a1dfde95db302ec
Christiandel Rabe <chris.rabe14@gmail.com>
2017-06-02 12:17:15 +1200

    connected the view with the gui

--- KPSServer/src/server/control/ServerControl.java
+++ KPSServer/src/server/control/ServerControl.java
@@ -1,9 +1,12 @@
 package server.control;
 
+import javax.swing.JTextArea;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
+import server.handlers.operations.Operations;
 import server.handlers.websocket.KPSWebSocketHandler;
 
 public class ServerControl {
@@ -11,20 +14,30 @@ public class ServerControl {
 	private int port = 8080;
 	private Server server;
 
-	public void start(String port) throws Exception{
+	private JTextArea logPanel = null;
+
+	public ServerControl(JTextArea logPanel) {
+		this.logPanel = logPanel;
+	}
+
+	public void start(String port) throws Exception {
 		if (port == null || !port.matches("[-+]?\\d*\\.?\\d+")) {
 			return;
 		}
 		this.port = Integer.parseInt(port);
 		this.server = new Server(this.port);
-		
-		WebSocketHandler wsHandler = new WebSocketHandler(){
+
+		if (logPanel != null) {
+			Operations.setLogPanel(logPanel);
+		}
+
+		WebSocketHandler wsHandler = new WebSocketHandler() {
 			@Override
-			public void configure(WebSocketServletFactory factory){
+			public void configure(WebSocketServletFactory factory) {
 				factory.register(KPSWebSocketHandler.class);
 			}
 		};
-		
+
 		server.setHandler(wsHandler);
 		server.start();
 		server.join();
--- KPSServer/src/views/controllers/HomeController.java
+++ KPSServer/src/views/controllers/HomeController.java
@@ -3,9 +3,12 @@ package views.controllers;
 import rabe_studios.components.resources.BaseResource;
 import rabe_studios.mvc.controllers.BaseViewController;
 import rabe_studios.mvc.views.View;
+import server.control.ServerControl;
 import views.pages.HomeView;
 
-public class HomeController extends BaseViewController{
+public class HomeController extends BaseViewController {
+
+	ServerControl server = null;
 
 	public HomeController(BaseResource resource) {
 		super(resource);
@@ -14,7 +17,14 @@ public class HomeController extends BaseViewController{
 	@Override
 	protected View createView() {
 		HomeView homeView = new HomeView();
-		// I usually add listeners here
+		homeView.addStartButtonListener(e -> {
+			server = new ServerControl(null);
+			try {
+				server.start(homeView.getPort());
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
 		return homeView;
 	}
 
