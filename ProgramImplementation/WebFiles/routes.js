274b6aff3f7361511735d9dd93b9ca08c174347e
Linus Go <golinu@ecs.vuw.ac.nz>
2017-06-06 16:00:49 +1200

    added objects

--- ProgramImplementation/WebFiles/routes.js
+++ ProgramImplementation/WebFiles/routes.js
@@ -1,3 +1,23 @@
 /**
  * Created by Edward on 6/06/2017.
  */
+
+/**
+* Object that represents a Route object, used for calculating the shortest routes.
+*
+*/
+function Route(origin, destination, tripDuration, gramPrice, cubicPrice){
+    this.origin = origin;
+    this.destination = destination;
+    this.tripDuration = tripDuration;
+    this.gramPrice = gramPrice;
+    this.cubicPrice = cubicPrice;
+};
+/**
+ * Represents a City object, which is represents a Node in the graph.
+ * @param name
+ * @constructor
+ */
+function City(name){
+    this.name = name;
+};
