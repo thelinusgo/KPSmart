86af6bc48866ff944af887ba7aabf340545ac3fd
Edward Kelly <ed7kelly@gmail.com>
2017-06-08 14:43:27 +1200

    setup some of the route finding stuff

--- ProgramImplementation/WebFiles/routes.js
+++ ProgramImplementation/WebFiles/routes.js
@@ -2,22 +2,32 @@
  * Created by Edward on 6/06/2017.
  */
 
+if (sessionStorage.getItem("routes") == null)sessionStorage.setItem("routes", []);
+var routes = sessionStorage.getItem("routes");
+
 /**
 * Object that represents a Route object, used for calculating the shortest routes.
 *
 */
-function Route(origin, destination, tripDuration, gramPrice, cubicPrice){
+function Route(origin, destination, transportFirm, transportType, pricePerGram, pricePerCC, departureDay, departsEvery, tripDuration){
     this.origin = origin;
     this.destination = destination;
+    this.transportFirm = transportFirm;
+    this.transportType = transportType;
+    this.pricePerGram = pricePerGram;
+    this.pricePerCC = pricePerCC;
+    this.departureDay = departureDay;
+    this.departsEvery = departsEvery;
     this.tripDuration = tripDuration;
-    this.gramPrice = gramPrice;
-    this.cubicPrice = cubicPrice;
 };
-/**
- * Represents a City object, which is represents a Node in the graph.
- * @param name
- * @constructor
- */
-function City(name){
-    this.name = name;
-};
+
+function DijkstraNode(city, fromNode, costToHere){
+    this.city = city;
+    this.fromNode = fromNode;
+    this.costToHere = costToHere;
+}
+
+function startSearch(origin, destination){
+    startNode = new DijkstraNode(origin, null, 0);
+    endNode = new DijkstraNode(destination, null, null);
+}
