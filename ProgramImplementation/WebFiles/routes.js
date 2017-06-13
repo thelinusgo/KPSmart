8373a5df6c36bbd0b090198667ea1e56244247e1
Jian Wei Chong <Jian Wei Chong>
2017-06-14 10:18:04 +1200

    Added bottom half of shortestPath function

--- ProgramImplementation/WebFiles/routes.js
+++ ProgramImplementation/WebFiles/routes.js
@@ -44,6 +44,41 @@ function Map() {
     this.addVertex = function(cityName, edges){
         this.vertices[cityName] = edges;
     }
+
+
+
+    while(!nodes.isEmpty()) {
+        smallest = nodes.dequeue();
+
+        if(smallest === finish) {
+            path = [];
+
+            while(previous[smallest]) {
+                path.push(smallest);
+                smallest = previous[smallest];
+            }
+
+            break;
+        }
+
+        if(!smallest || distances[smallest] === INFINITY){
+            continue;
+        }
+
+        for(neighbor in this.vertices[smallest]) {
+            alt = distances[smallest] + this.vertices[smallest][neighbor];
+
+            if(alt < distances[neighbor]) {
+                distances[neighbor] = alt;
+                previous[neighbor] = smallest;
+
+                nodes.enqueue(alt, neighbor);
+            }
+        }
+    }
+
+    return path;
+};
 }
 
 /**
