7af24950af7393e00e903c96ee69dfec73470fa0
Christiandel Rabe <chris.rabe14@gmail.com>
2017-06-15 22:01:17 +1200

    added jackson library

---
+++ KPSServer/src/server/model/handlers/data/JSONReader.java
@@ -0,0 +1,15 @@
+package server.model.handlers.data;
+
+import java.io.File;
+
+import server.model.database.Database;
+
+public class JSONReader extends DataReader {
+
+	@Override
+	public Database read(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
